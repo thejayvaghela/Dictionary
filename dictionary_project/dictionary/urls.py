1258153aeb16cee69e50a5e3044c413ee49bb912
thejayvaghela <58729034+thejayvaghela@users.noreply.github.com>
2020-11-03 20:46:50 +0530

    Update urls.py

--- dictionary_project/dictionary/urls.py
+++ dictionary_project/dictionary/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from dictionary_project import settings
 from dictionary.views import dict_home, word, word_mean
-
+#urls
 urlpatterns = [
     url('dict_home/$', dict_home),
     path('word_mean/<words>/', word_mean),
